944dd7b8ad91e7bf81dee89d927556536b2461e1
dotchamp <ganeshjadhavar2003@gmail.com>
2024-03-12 03:38:56 +0530

    completed

--- E-Commerce-Spring-Boot-main/E-Commerce-Spring-Boot-main/src/main/java/com/dbms/triplehao/dao/RegionStoreDao.java
+++
@@ -1,9 +0,0 @@
-package com.dbms.triplehao.dao;
-
-import com.dbms.triplehao.entity.RegionStore;
-
-import java.util.List;
-
-public interface RegionStoreDao {
-    List<RegionStore> queryRegionStore();
-}
--- E-Commerce-Spring-Boot-main/E-Commerce-Spring-Boot-main/src/main/java/com/dbms/triplehao/service/RegionStoreService.java
+++
@@ -1,9 +0,0 @@
-package com.dbms.triplehao.service;
-
-import com.dbms.triplehao.entity.RegionStore;
-
-import java.util.List;
-
-public interface RegionStoreService {
-    List<RegionStore> getRegionStoreList();
-}
